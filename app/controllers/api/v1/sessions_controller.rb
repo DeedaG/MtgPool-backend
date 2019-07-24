60f4008f2b5697b6211a6ab2cf75430288c1f50e
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-23 22:02:10 -0500

    created current user and create methods

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,25 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    binding.pry
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id]= @user.id
+        render json: @user
+      else
+        # binding.pry
+        render json: {
+          error: "Invalid Credentials"
+        }
+      end
+    end
+
+    def get_current_user
+      if logged_in?
+        render json: current_user
+      else
+        render json: {
+          error: "Not Logged In"
+        }
+      end
+    end
 end

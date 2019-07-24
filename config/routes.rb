7ba1ec5b12f7046997a04c34e4efa2aa78a338e9
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-23 22:06:55 -0500

    created get and post routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :investors
-  resources :pools
-  resources :loans
+
+post "/api/v1/login", to: "api/v1/sessions#create"
+get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :investors
+      resources :pools
+      resources :loans
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

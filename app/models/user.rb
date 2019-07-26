eb782003a78f2f23d0a4a9b8ef2475d496ce987c
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-26 12:11:07 -0500

    user has many pools

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :investors
-  has_many :pools, through: :investors
+  has_many :pools
   has_many :loans, through: :pools
-
+  has_many :investors, through: :pools
 end

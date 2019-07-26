aae895d39407be1d3c0da4bec6933c4adf6e3826
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-26 12:08:06 -0500

    added crud methods and fastjsonapi

--- app/controllers/api/v1/loans_controller.rb
+++ app/controllers/api/v1/loans_controller.rb
@@ -1,3 +1,51 @@
 class LoansController < ApplicationController
-  
+  before_action :set_loan, only: [:show, :update, :destroy]
+
+
+  def index
+    @loans = Loan.all
+    render json: @loans
+  end
+
+  def show
+     # render json: @loan
+    loan_json = LoanSerializer.new(@loan).serialized_json
+    render json: loan_json
+  end
+
+  def create
+    @loan = Loan.new(loan_params)
+
+    if @loan.save
+      render json: @loan, status: :created, location: @loan
+    else
+      render json: @loan.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def update
+    if @loan.update(loan_params)
+      render json: @loan
+    else
+      render json: @loan.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @loan.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_loan
+      @loan = Loan.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def loan_params
+      params.require(:loan).permit(:borrower, :term, :amount, :rate, :close_date)
+    end
+
 end

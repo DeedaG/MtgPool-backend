506d224070658daaf217f1599f7f4f01a0adfca0
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-26 12:09:38 -0500

    corrected fastjsonapi documentation

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
+
   def create
     @user = User.find_by(username: params[:session][:username])
 #    binding.pry
       if @user && @user.authenticate(params[:session][:password])
         session[:user_id]= @user.id
-        render json: @user
+        render json: UserSerializer.new(@user), status: :ok
       else
         # binding.pry
         render json: {
@@ -15,7 +16,9 @@ class Api::V1::SessionsController < ApplicationController
 
     def get_current_user
       if logged_in?
-        render json: current_user
+        # user_json = UserSerializer.new(current_user).serialized_json
+        render json: UserSerializer.new(current_user)
+        # render json: user_json
       else
         render json: {
           error: "Not Logged In"
@@ -27,7 +30,6 @@ class Api::V1::SessionsController < ApplicationController
       session.clear
       render json: {
         notice: "succesfully logged out"
-      }, status: ok
+      }, status: :ok
     end
-
 end

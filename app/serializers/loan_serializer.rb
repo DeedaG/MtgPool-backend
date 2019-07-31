56af6669c89e9dae6246bd77762833065d3945a1
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-30 22:02:54 -0500

    testing necessary attributes

--- app/serializers/loan_serializer.rb
+++ app/serializers/loan_serializer.rb
@@ -1,6 +1,6 @@
 class LoanSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :borrower, :amount, :close_date
+  attributes :borrower, :amount, :close_date, :term, :rate
 
   belongs_to :pool
 end

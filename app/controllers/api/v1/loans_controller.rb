4e0c2517e2162789bcab42cee97ae1b62994b24a
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-30 22:01:41 -0500

    added fastjsonapi serializer code

--- app/controllers/api/v1/loans_controller.rb
+++ app/controllers/api/v1/loans_controller.rb
@@ -1,23 +1,24 @@
-class LoansController < ApplicationController
+class Api::V1::LoansController < ApplicationController
   before_action :set_loan, only: [:show, :update, :destroy]
 
 
   def index
     @loans = Loan.all
-    render json: @loans
+    # render json: @loans
+    loan_json = LoanSerializer.new(@loans).serialized_json
+    render json: loan_json
   end
 
   def show
-     # render json: @loan
-    loan_json = LoanSerializer.new(@loan).serialized_json
-    render json: loan_json
+      render json: @loan
+
   end
 
   def create
     @loan = Loan.new(loan_params)
 
     if @loan.save
-      render json: @loan, status: :created, location: @loan
+      render json: @loan, status: :ok
     else
       render json: @loan.errors, status: :unprocessable_entity
     end

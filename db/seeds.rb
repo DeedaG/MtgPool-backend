d526c4066523b33584214dac794e0674034eba49
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-07-30 22:03:55 -0500

    seeded two loans

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,7 @@
   fancyBank = Investor.first
  # pool1 = fancyBank.pools.create(name: "pool1", pool_amount: "500,000")
  pool1 = Pool.first
- loan1 = pool1.loans.create(borrower: "Smith", term: "30", amount: "100,000", rate: "5", close_date: "8/15/19")
+ # loan1 = pool1.loans.create(borrower: "Smith", term: "30", amount: "100,000", rate: "5", close_date: "8/15/19")
+ # loan2 = pool1.loans.create(borrower: "King", term: "30", amount: "200,000", rate: "4.5", close_date: "9/01/19")
+ # loan3 = pool1.loans.create(borrower: "King", term: "30", amount: "200,000", rate: "4.5", close_date: "9/01/19")
+ 

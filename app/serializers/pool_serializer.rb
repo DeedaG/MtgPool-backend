6a4ca8060170abb8e7e4e175c1b60d2e0564d8ad
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-08-01 17:13:33 -0500

    did not save changes, checking investor id error

--- app/serializers/pool_serializer.rb
+++ app/serializers/pool_serializer.rb
@@ -1,11 +1,11 @@
 class PoolSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :pool_amount, :investor_id
+  attributes :name, :pool_amount, :investor_id, :loans
 
 
 
   belongs_to :investor
   belongs_to :user
-  # has_many :loans, serializer: LoanSerializer
+  has_many :loans, serializer: LoanSerializer
 
 end

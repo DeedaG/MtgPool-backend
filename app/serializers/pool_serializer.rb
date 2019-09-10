44396dce596da4b619e0a0074ab1cd3183d34d26
Christia Tidwell <ChristiaTidwell@gmail.com>
2019-09-10 15:58:56 -0500

    considering relationship manipulation of pool and loans

--- app/serializers/pool_serializer.rb
+++ app/serializers/pool_serializer.rb
@@ -2,7 +2,9 @@ class PoolSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :pool_amount, :investor_id, :loans
 
-
+  def loans
+    loans.map(loan => loan.pool_id = pool.id)
+  end
 
   belongs_to :investor
   belongs_to :user
